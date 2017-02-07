e9ec34b4d8cef602d530d0c0b3ff63ad049c2e36
Jon Secchis <jon@jon.work>
2017-02-07 14:53:46 -0200

    Moving files

--- 1.1.3-EvaluatingCombinations.scm
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env gsi-script
-
-;The interpreter itself applies a recursive procedure to evaluate expressions.
-;It applies the procedures starting from inner subexpressions and following outwards.
-;This is an example of a 'tree accumulation' process.
-
-;Example
-(pretty-print
-(*(+ 2 (* 4 6))(+ 3 5 7)))
